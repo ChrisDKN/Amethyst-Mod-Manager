a37d5e0b41e7229c66ecc3320dea4851380c4a5a
Chris <71672224+ChrisDKN@users.noreply.github.com>
2026-09-24 13:58:52 +0100

    New translations amethyst_en.ts (Portuguese, Brazilian)
    [ci skip]

--- Localisation/amethyst_pt_BR.ts
+++ Localisation/amethyst_pt_BR.ts
@@ -1891,22 +1891,6 @@ O cache md5 é preservado. Os arquivos serão baixados novamente conforme necess
   <context>
     <name>ChangeVersionView</name>
     <message>
-      <source>File</source>
-      <translation>Arquivo</translation>
-    </message>
-    <message>
-      <source>Version</source>
-      <translation>Versão</translation>
-    </message>
-    <message>
-      <source>Category</source>
-      <translation>Categoria</translation>
-    </message>
-    <message>
-      <source>Size</source>
-      <translation>Tamanho</translation>
-    </message>
-    <message>
       <source>Change Version - {0}</source>
       <translation>Mude a versão - {0}</translation>
     </message>
@@ -1949,6 +1933,50 @@ O cache md5 é preservado. Os arquivos serão baixados novamente conforme necess
     <message>
       <source>Install</source>
       <translation>Instalar</translation>
+    </message>
+    <message>
+      <source>Main files</source>
+      <translation>Arquivos principais</translation>
+    </message>
+    <message>
+      <source>Update files</source>
+      <translation>Atualizar arquivos</translation>
+    </message>
+    <message>
+      <source>Optional files</source>
+      <translation>Arquivos opcionais</translation>
+    </message>
+    <message>
+      <source>Miscellaneous files</source>
+      <translation>Arquivos diversos</translation>
+    </message>
+    <message>
+      <source>Old versions</source>
+      <translation>Versões anteriores</translation>
+    </message>
+    <message>
+      <source>Archived files</source>
+      <translation>Arquivos arquivados</translation>
+    </message>
+    <message>
+      <source>Other files</source>
+      <translation>Outros arquivos</translation>
+    </message>
+    <message>
+      <source>{0} ({1})</source>
+      <translation>{0} ({1})</translation>
+    </message>
+    <message>
+      <source>installed</source>
+      <translation>Instalado</translation>
+    </message>
+    <message>
+      <source>Version {0}</source>
+      <translation>Versão {0}</translation>
+    </message>
+    <message>
+      <source>No description provided.</source>
+      <translation>Nenhuma descrição fornecida.</translation>
     </message>
     <message>
       <source>Waiting for the browser download of '{0}' - click Cancel to stop.</source>
@@ -4939,6 +4967,14 @@ Quando terminar, o aplicativo muda para o novo perfil. Então volte aqui e press
   </context>
   <context>
     <name>DownloadStatusWidget</name>
+    <message>
+      <source>Downloading + installing</source>
+      <translation>Baixando + instalando</translation>
+    </message>
+    <message>
+      <source>{0} downloading + installing</source>
+      <translation>Baixando {0} + instalar</translation>
+    </message>
     <message>
       <source>{0} downloading</source>
       <translation>Baixando {0}</translation>
@@ -8284,6 +8320,14 @@ Aguardando o arquivo do instalador concluído nos seus locais de download. Você
   </context>
   <context>
     <name>MainWindow</name>
+    <message>
+      <source>Updating Filegraph…</source>
+      <translation>Atualizando Filegraph…</translation>
+    </message>
+    <message>
+      <source>Scanning Filegraph: {0}/{1} mods</source>
+      <translation>Verificando Filegraph: mods {0}/{1}</translation>
+    </message>
     <message>
       <source>Download</source>
       <translation>BAIXAR</translation>
@@ -17137,12 +17181,24 @@ Isso não pode ser desfeito.</translation>
       <translation>Baixar o Script Extender</translation>
     </message>
     <message>
+      <source>Nexus Premium users download automatically. For free users, the download page opens in your browser. Choose Manual Download for the Steam file. The wizard detects it when it finishes, or you can use Browse….</source>
+      <translation>O download automático dos usuários Premium Nexus. Para usuários gratuitos, a página do download abre em seu navegador. Escolha Download Manual para o arquivo Steam. O assistente o detecta quando termina, ou você pode usar a Navegação….</translation>
+    </message>
+    <message>
       <source>Browse…</source>
       <translation>Procurar…</translation>
     </message>
     <message>
       <source>Next →</source>
       <translation>Próximo →</translation>
+    </message>
+    <message>
+      <source>Checking Nexus download…</source>
+      <translation>Verificando o download Nexus…</translation>
+    </message>
+    <message>
+      <source>Script Extender</source>
+      <translation>Extensor de script</translation>
     </message>
     <message>
       <source>Fetching release from GitHub…</source>
@@ -23867,6 +23923,10 @@ Publicar faz com que essa revisão seja instalada pelos usuários. Não pode ser
     <message>
       <source>View</source>
       <translation>Visualizar</translation>
+    </message>
+    <message>
+      <source>Enable</source>
+      <translation>Habilitado</translation>
     </message>
     <message>
       <source>Install</source>
